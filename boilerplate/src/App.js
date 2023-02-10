0b68cd5464b3d827aac584dfd3431c4e550aa55c
Sudhanshu Tomar <76549125+SudhanshuTomar@users.noreply.github.com>
2023-02-10 23:44:59 +0530

    Update App.js

--- boilerplate/src/App.js
+++ boilerplate/src/App.js
@@ -2,60 +2,25 @@ import { useState, useRef } from "react";
 
 function App() {
   const [randomInput, setRandomInput] = useState("");
-  const [seconds, setSeconds] = useState(0);
 
-  const renders = useRef(0);
-  const inputRef = useRef();
-  const timerId = useRef();
+  const renders = 0;
 
   const handleChange = (e) => {
     setRandomInput(e.target.value);
-    renders.current++;
-  };
-
-  const startTimer = () => {
-    timerId.current = setInterval(() => {
-      renders.current++;
-      setSeconds((prev) => prev + 1);
-    }, 1000);
-    inputRef.current.focus();
-  };
-
-  const stopTimer = () => {
-    clearInterval(timerId.current);
-    timerId.current = 0;
-    inputRef.current.focus();
-  };
-
-  const resetTimer = () => {
-    stopTimer();
-    if (seconds) {
-      renders.current++;
-      setSeconds(0);
-    }
-    inputRef.current.focus();
+    renders = renders+1;
   };
 
   return (
     <main className="App">
       <input
-        ref={inputRef}
         type="text"
         value={randomInput}
         placeholder="Random Input"
         onChange={handleChange}
       />
-      <p>Renders: {renders.current}</p>
+      <p>Renders: {renders}</p>
       <br />
       <br />
-      <section>
-        <button onClick={startTimer}>Start</button>
-        <button onClick={stopTimer}>Stop</button>
-        <button onClick={resetTimer}>Reset</button>
-      </section>
-      <br />
-      <br />
-      <p>Seconds: {seconds}</p>
       <br />
       <br />
       <p>{randomInput}</p>
